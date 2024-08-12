560dd9d816d68a39456033d9442fde81c450600d
Johan Brits <jjohanbrits@gmail.com>
2024-08-12 21:12:00 +0200

    Fix heroku script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // IMPORTS
 const express = require("express");
+const path = require('path');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
